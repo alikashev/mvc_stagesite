16bf394005cacab820a88a1ae2e11afd839ed82e
Ali Kashev <57992892+alikashev@users.noreply.github.com>
2022-03-22 14:52:14 +0100

    updated db

--- db/stagesite.sql
+++ db/stagesite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 22 mrt 2022 om 10:15
+-- Gegenereerd op: 22 mrt 2022 om 14:51
 -- Serverversie: 10.4.22-MariaDB
 -- PHP-versie: 8.1.1
 
@@ -42,17 +42,8 @@ CREATE TABLE `bestanden` (
 --
 
 INSERT INTO `bestanden` (`id`, `naam`, `upload_datum`, `bestand_data`, `bestand_path`, `uploader_id`, `bestand_omschrijving`) VALUES
-(6, 'testtttt', '2022-03-21 11:22:43', '', '', 1, ''),
-(7, 'asdd', '2022-03-21 11:23:59', '', '', 1, ''),
-(8, '2913-pdf-test.pdf', '2022-03-21 11:34:27', '', '', 1, ''),
-(9, '8142-pdf-test.pdf', '2022-03-21 11:38:26', '', '', 1, ''),
-(10, '1635-pdf-test.pdf', '2022-03-21 11:38:39', '', '', 1, ''),
-(11, '5663-pdf-test.pdf', '2022-03-21 11:42:38', '', '', 1, ''),
-(12, '7091-pdf-test.pdf', '2022-03-21 11:43:21', '', '', 1, ''),
-(13, '6281-pdf-test.pdf', '2022-03-21 11:44:09', '', '', 1, ''),
-(14, '9582-pdf-test.pdf', '2022-03-21 11:46:26', '', '', 1, 'Dit is een tninsidns'),
-(15, '2469-pdf-test.pdf', '2022-03-21 11:49:17', '', '', 1, 'Dit is een tninsidns'),
-(16, '9817-test.pdf', '2022-03-22 10:08:58', '', '', 1, 'asd');
+(20, '1918-test.pdf', '2022-03-22 10:55:31', '', 'mvc_stagesite\\uploads/', 1, 'test1212'),
+(21, '7425-test.pdf', '2022-03-22 10:56:22', '', 'mvc_stagesite\\uploads/', 1, 'test1212');
 
 -- --------------------------------------------------------
 
@@ -204,7 +195,7 @@ ALTER TABLE `stage_bedrijven`
 -- AUTO_INCREMENT voor een tabel `bestanden`
 --
 ALTER TABLE `bestanden`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT voor een tabel `gebruikers`
