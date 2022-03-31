70a97f6c0e3fc6984f71e4b87a9e9be7c357d537
Layla <90128114+Lulunade@users.noreply.github.com>
2022-03-31 12:11:17 +0200

    Inloggen beveiligd en sessions werkend

--- controller/Login.php
+++ controller/Login.php
@@ -4,40 +4,47 @@ require_once 'View/outputData.php';
 
 class Login
 {
-  public function __construct()
-  {
-    $this->User = new User();
-    $this->outputData = new OutputData();
-  }
-
-  public function Index()
-  {
-    include_once 'View/login.php';
-  }
-
-  public function checkLogin()
-  {
-    var_dump($_POST);
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $user = $this->User->readOneUserByEmail($email);
-    $user = $user[0];
-    if (empty($user->wachtwoord_hash)) {
-      return false;
-    } else {
-      if ($user) {
-        if (password_verify($password, $user->wachtwoord_hash)) {
-          header('Location: ../checkAccountType/' . $user->id);
-        } else {
-          return false;
-        }
-      }
+    public function __construct()
+    {
+        $this->User = new User();
+        $this->outputData = new OutputData();
     }
-  }
 
-  public function checkaccountType($id)
-  {
-    $user = $this->User->readOneUser($id);
-    var_dump($user);
-  }
+    public function Index()
+    {
+        include_once 'View/login.php';
+    }
+
+    public function checkLogin()
+    {
+        session_start();
+        var_dump($_SESSION);
+        var_dump($_POST);
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $user = $this->User->readOneUserByEmail($email);
+        $user = $user[0];
+        if (empty($user->wachtwoord_hash)) {
+            return false;
+        } else {
+            if ($user) {
+                if (password_verify($password, $user->wachtwoord_hash)) {
+                    $_SESSION['user'] = $user->id;
+                    header('Location: ../checkAccountType/' . $user->id);
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    public function checkaccountType($id)
+    {
+        session_start();
+        if ($_SESSION["user"] !== 0) {
+            $user = $this->User->readOneUser($id);
+            var_dump($user);
+            var_dump($_SESSION);
+        }
+    }
 }
